f611f27cbfbb4942849e674f0287fc6e8c86695a
Blake Danson <blakedanson@gmail.com>
2019-02-20 08:20:30 -0800

    Add onDrop file method

--- client/src/containers/ImageDropContainer/index.js
+++ client/src/containers/ImageDropContainer/index.js
@@ -3,15 +3,47 @@ import Dropzone from 'react-dropzone';
 import DropInput from './DropInput';
 import './ImageDropContainer.css';
 
-export default function ImageDropContainer() {
-  return (
-    <div className="dropContainer">
-      <p>Drop Image Here:</p>
-      <div className="dropInput">
-        <Dropzone accept="image/*" className="dropZone">
-          {DropInput}
-        </Dropzone>
+class ImageDropContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: '',
+    };
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(acceptedFiles) {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const fileAsBinaryString = reader.result;
+        this.setState({ file: fileAsBinaryString });
+        console.log('should have loaded', this.state.file);
+        // do whatever you want with the file content
+      };
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+
+      reader.readAsBinaryString(file);
+    });
+  }
+
+  render() {
+    return (
+      <div className="dropContainer">
+        <p>Drop Image Here:</p>
+        <div className="dropInput">
+          <Dropzone
+            accept="image/*"
+            className="dropZone"
+            onDrop={this.onDrop}
+          >
+            {DropInput}
+          </Dropzone>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default ImageDropContainer;
